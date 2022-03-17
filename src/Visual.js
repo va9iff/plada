82453d57f7c46d523584e3d8e73586e19dd914c1
va9iff <va9iff@gmail.com>
2022-03-17 16:37:44 +0400

    centered visual by subtracting radius from position in javascript

--- src/Visual.js
+++ src/Visual.js
@@ -12,11 +12,11 @@ export class Visual extends Kinematic {
 		document.body.appendChild(this.element)
 	}
 	visualFix() {
-		this.element.style.left = this.position.x + "px"
-		this.element.style.top = this.position.y + "px"
+		this.element.style.left = `${this.position.x - this.radius}px`
+		this.element.style.top = `${this.position.y - this.radius}px`
 
-		this.element.style.width = this.radius * 2 + "px"
-		this.element.style.height = this.radius * 2 + "px"
+		this.element.style.width = `${this.radius * 2}px`
+		this.element.style.height = `${this.radius * 2}px`
 
 		this.color ? (this.element.style.backgroundColor = this.color) : null
 		// if there is no color, don't give inline, let the css class handle
