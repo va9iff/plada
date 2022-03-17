77f06b277050f570cfdd3dab6da2b6231ff3e278
va9iff <va9iff@gmail.com>
2022-03-17 11:15:23 +0400

    superior collision listener adder for o2o

--- src/Body.js
+++ src/Body.js
@@ -5,38 +5,43 @@ import { Visual } from "./Visual.js"
 // it will fire .parents' .collision() on its own collision.
 // as the collision will fire .parent's collision too, it's recursive by nature.
 
-	// A.collide(B) = ()=>{}
-	// A.collide(B).end = ()=>{}
+// A.collide(B) = ()=>{}
+// A.collide(B).end = ()=>{}
 
 export class Body extends Visual {
-	constructor(){
+	constructor() {
 		super(...arguments)
 		this.listenings = []
 	}
 
-
-	static c2cbasic = [Body]
-	static hasThisClassListener(cls){
-		console.log(this.c2cbasic.some((l)=>l==cls))
+	static c2c = []
+	static hasThisClassListener(cls) {
+		return this.c2c.some(l => l.cls == cls)
 		// return console.log()
 	}
-	static collide(cls){
-		// if (){}
-		let c2cListener = !
-		{
-			cls: cls
-		}
-		this.c2cbasic.push()
+	static alreadyExistingListener(cls) {
+		return this.c2c.filter(e => e.cls == cls)[0]
+	}
+	static collide(cls) {
+		let c2cListener = this.hasThisClassListener(cls)
+			? this.alreadyExistingListener()
+			: {
+					cls: cls,
+					start: () => {},
+					end: () => {},
+					during: () => {},
+			  }
+		this.c2c.push(c2cListener)
+		return c2cListener
 	}
 
-
-	addCollision(body, collideFun=(self, body)=>{}, collType = "during") {
+	addCollision(body, collideFun = (self, body) => {}, collType = "during") {
 		let listening = {
 			body: body,
 			wasColliding: false,
-			start: ()=>{},
-			end: ()=>{},
-			during: ()=>{}
+			start: () => {},
+			end: () => {},
+			during: () => {},
 		}
 		listening[collType] = collideFun
 		this.listenings.push(listening)
@@ -51,8 +56,25 @@ export class Body extends Visual {
 		// console.log(this.listenings)
 		for (let listening of this.listenings) {
 			// console.log(this.listenings[body])
-			if (this.isColliding(listening.body)) listening.during(this, listening.body)
+			if (!listening.wasColliding && this.isColliding(listening.body)) 
+				{
+					listening.start(this, listening.body)
+					listening.wasColliding = true
+				}
+			if (this.isColliding(listening.body))
+				{
+					listening.during(this, listening.body)
+					listening.wasColliding = true
+				}
+			if (listening.wasColliding && !this.isColliding(listening.body)) 
+				{
+					listening.end(this, listening.body)
+					listening.wasColliding = false
+				}
 		}
+	}
+	static runClassLevelCollisions(){
+		for(let listening of this.c2c){}
 	}
 	devFrame(delta) {
 		super.devFrame(delta)
@@ -61,6 +83,3 @@ export class Body extends Visual {
 	// obj-to-obj collision binding control object
 	// get collide(){}
 }
-
-
-Body.hasThisClassListener(Body)
--- src/index.js
+++ src/index.js
@@ -9,19 +9,20 @@ import { Visual } from "./Visual.js"
 
 window.k = new Body()
 window.b = new Body()
-let r = new Body()
-r.x = 150
+// let r = new Body()
+// r.x = 150
 b.position.x = 100
 k.velocity.x = 0.02
 // k.frame = (d)=> console.log(k.isColliding(b))
-// k.addCollision(b,()=>console.log('afsdjf'), "during")
-// k.addCollision(r,()=>console.log('r'))
+k.addCollision(b,()=>console.log('s'), "start")
+k.addCollision(b,()=>console.log('d'), "during")
+k.addCollision(b,()=>console.log('e'), "end")
 
-Body.collide[Body] = ()=>{}
+// Body.collide[Body] = ()=>{}
 
 k.position.x = -50
 
-console.log(k)
-console.log(Body)
+// console.log(k)
+// console.log(Body)
 
 window.Point = Point
